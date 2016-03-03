40c9cc88055aedb24e1bb2b75807d9d659e0b163
jtougas <jtougas@stack8.com>
2016-03-03 12:59:41 -0500

    123123123

--- provision-root.sh
+++ provision-root.sh
@@ -10,23 +10,3 @@ apt-get -y install git
 # uses su to ensure that the setup script is executed by the vagrant user because the Vagrant Shell provisioner runs in 
 # a sudo environment which causes the script to be executed by the root user
 su -c "/vagrant/provision-vagrant.sh" vagrant
-
-#last pass cli
-cd ~
-apt-get -y install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip
-git clone https://github.com/lastpass/lastpass-cli.git
-cd ~/lastpass-cli
-make
-make install
-
-#fzf
-cd ~
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-#copy .profile
-cd ~
-
-su -c "cp /vagrant/.profile ~/.profile" vagrant
-source .profile
-
